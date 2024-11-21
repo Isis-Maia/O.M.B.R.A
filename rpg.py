47f2c540da06dd858ef7e038fc7fc043b144b202
Isis-Maia <149488519+Isis-Maia@users.noreply.github.com>
2024-11-21 01:27:31 -0300

    Initial commit

---
+++ rpg.py
@@ -0,0 +1,47 @@
+print("PROJETO DE SELEÇÃO")
+nome = input("Insira seu nome: ").title()
+inicio = input("Insira a senha: ")
+
+if inicio == "AruãPiyg":
+    print("Perfeito")
+    print(f"Olá {nome}, seja bem-vindo ao seu novo emprego!")
+    print("Antes de passarmos seu primeiro trabalho, preciamos passar algumas informações muito importantes.")
+    regras =   input("Você já leu as regras? S/N ").upper()
+
+    if regras == "N":
+        print("Loading…")
+
+        print("█▒▒▒▒▒▒▒▒▒")
+        print("10%")
+        print("███▒▒▒▒▒▒▒")
+        print("30%")
+        print("█████▒▒▒▒▒")
+        print("50%")
+        print("███████▒▒▒")
+        print("100%")
+        print("██████████")
+        print("           ")
+        input("Pressione enter para continuar. . .")
+
+        print(f"A O.M.B.R.A tambem conhecida como 'Organização de Monitoramento e Batalha de Risco Anômalos', está com o novo projeto, o qual você, {nome}, está inserido, o projeto F.O.L.C.L.O.R.E.")
+        input("Pressione enter para continuar. . .")
+        
+        print("Precisamos que informe sua área para continuar o processo de seleção.")
+        print("           ")
+        print("Abatedores: Abatedores são os chamados para eliminar os Anômalos ou levalos para a zona de segurança.")
+        print("           ")
+        print("Analistas: Responsaveis pelo estudo e monitoramento dos Anômalos.")
+        print("           ")
+        print("Teurgos: Responsaveis por realizar os rituais e encantamentos para controlar os Anômalos.")
+        print("           ")
+        print("Vigilantes: Responaveis por monitorar qualquer atividade anomala no Brasil e informar a agencia.")
+        print("           ")
+        print("Silenciadores: Responsaveis por eliminar vestigios e encombrir qualquer vestigio sobre a O.M.B.R.A. ou os Anômalos.")
+        print("           ")
+
+
+else:
+    print("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄")
+    print("████▌▄▌▄▐▐▌█████")
+    print("████▌▄▌▄▐▐▌▀████")
+    print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
